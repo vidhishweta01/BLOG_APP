073b53cae5239bf0e328fc834bebda0f9339ecf7
Shweta Srivastava <73999611+vidhishweta01@users.noreply.github.com>
2021-05-14 22:00:33 +0530

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -56,15 +56,13 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def search
-    def search  
-      if params[:search].blank?  
+  def search  
+    if params[:search].blank?  
         redirect_to(root_path, alert: "Empty field!") and return  
       else  
         @parameter = params[:search].downcase  
         @article = Article.all.where("lower(Title) LIKE :search", search: "%#{@parameter}%")  
       end  
-    end
   end
 
   private
